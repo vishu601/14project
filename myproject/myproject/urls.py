3344e61c0604418076f4b901187bdf1d607cbf35
vishu601 <vishu601@gmail.com>
2026-07-20 11:42:04 +0000

    fixed 404 error by adding root redirect to login

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect  # <-- Ye import zaroori hai
 from app1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Ye line add kar di jo root URL ('') ko seedha login page par redirect kar degi
+    path('', lambda request: redirect('login'), name='home'),
+    
     path('StudentReg/', StudentReg, name='StudentReg'),
     path('showstudent/', showstudent, name='showstudent'),
     path('updatestudent/<int:id>/', updatestudent, name='updatestudent'),
